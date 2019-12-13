cc01f3be894ab11e1339e9232ec7942d2aa9aff1
Wellington Rodrigues <wellington.rodrigues@algosoft.com.br>
2019-12-13 14:53:47 -0300

    added apiKey and secretKey

--- xchange-core/src/main/java/org/knowm/xchange/service/trade/TradeService.java
+++ xchange-core/src/main/java/org/knowm/xchange/service/trade/TradeService.java
@@ -118,7 +118,7 @@ public interface TradeService extends BaseService {
   }
 
   default String placeMarketOrder(MarketOrder marketOrder, String apiKey, String secretKey)
-          throws IOException {
+      throws IOException {
     throw new NotYetImplementedForExchangeException();
   }
 
@@ -270,6 +270,11 @@ public interface TradeService extends BaseService {
    */
   default UserTrades getTradeHistory(TradeHistoryParams params) throws IOException {
     throw new NotYetImplementedForExchangeException("getTradeHistory");
+  }
+
+  default UserTrades getTradeHistory(TradeHistoryParams params, String apiKey, String secretKey)
+      throws IOException {
+    throw new NotYetImplementedForExchangeException();
   }
 
   default UserTrades getTradeHistory(TradeHistoryParams params, String apiKey, String secretKey)
